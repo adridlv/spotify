42bc21cc2bad14fbf8a8d84e8b93f031915ffa29
adridlv <adriandlv53@gmail.com>
2016-07-01 16:44:50 +0200

    terminado, falta css

--- js/index.js
+++ js/index.js
@@ -14,11 +14,28 @@ function requestAjax (searchID, call){
 		dataType: "json",
 		success: function (response){
 			if(call == "artist"){
-				var artistID = response.artists.items[0].id;
+				$('.container-artist').empty();
+				response.artists.items.forEach(function(artist){
+					console.log(artist);
+					var element = $("<div class='artist'>");
+					var nameArtist = $("<h1 class='name-artist'>").text(artist.name);
+					element.append(nameArtist);
+					console.log(artist.images[0].url);
+					var imageArtist = $("<img class='image-artist'>").attr("src", artist.images[0].url);
+					element.append(imageArtist);
+					var artistID = artist.id;
+					element.on("click", function(){
+						requestAjax(artistID, "album");
+					});
+
+					$('.container-artist').append(element);
+				});
+				/*var artistID = response.artists.items[0].id;
 				var element = $(".container-artist");
-				requestAjax(artistID, "album");
+				requestAjax(artistID, "album");*/
 
 			}else if(call == "album"){
+				$('.container-album').empty();
 				response.items.forEach(function (album){
 					var elementAux = $("<div class='album'>");
 					var imageAlbum = $("<img class='image-album'>").attr("src", album.images[0].url);
@@ -30,18 +47,39 @@ function requestAjax (searchID, call){
 				});
 
 				//element.append(elementAux);
-				$(".container-artist").append(elementAux);
+				$(".container-album").append(elementAux);
 			});
 
-			}else{
+			}else if(call == "track"){
 				var elementAux = $("<ul class='track-list'>");
-
 				response.tracks.items.forEach(function(song){
 					var songElement = $("<li class='track-song'>");
-					var songLink = $("<a class='link-song'>").attr("href", song.preview_url);
-					songLink.attr("target", "_blank");
+					
+					var songLink = $("<p class='link-song' style='display:inline-block;margin-right:1em;'>");
+					var songFavIcon = $("<a class='btn-floating btn-small waves-effect waves-light lightgreen' style='float:right;'>");
+					var songIcon = $("<i class='material-icons'>").text("add");
+					songFavIcon.append(songIcon);
+					//var songFavIcon = $("<i class='fa fa-plus-circle' style='display:inline-block;' 'aria-hidden='true'></i>")
+					
+					songFavIcon.on("click", function(){
+						//var songElement = {"name": song.name, "uri": song.uri};
+						var listSongs = JSON.parse(window.localStorage.getItem('song-list')) || {};
+						var currentUser = JSON.parse(window.localStorage.getItem('userLogin'));
+
+						var userListSong = listSongs[currentUser.name] || {};
+						userListSong[song.name] = song.uri;
+						listSongs[currentUser.name] = userListSong;
+
+						window.localStorage.setItem("song-list", JSON.stringify(listSongs));
+					});
+
 					songLink.text(song.name);
+					songLink.on("click", function(){
+						$('iframe').attr("src", "https://embed.spotify.com/?uri="+song.uri);
+					});
 					songElement.append(songLink);
+					songElement.append(songFavIcon);
+					
 					elementAux.append(songElement);
 
 				});
@@ -55,76 +93,25 @@ function requestAjax (searchID, call){
 	});
 }
 
-function requestArtist (artist){
-	$.ajax({
-		url: "https://api.spotify.com/v1/search?type=artist&query="+artist,
-		dataType: "json",
-		success: function (response){
-			var artistID = response.artists.items[0].id;
-			var element = $(".container-artist");
-			requestAlbum(artistID,element);
-		},
-		error: function (response){
-			console.log(response);
-		}
-	});
-}
+function getFavouriteList(){
+	var listSongs = JSON.parse(window.localStorage.getItem('song-list')) || {};
+	var currentUser = JSON.parse(window.localStorage.getItem('userLogin'));
 
-function requestAlbum (artistID, element){
-	$.ajax({
-		url: "https://api.spotify.com/v1/artists/"+artistID+"/albums?market=ES",
-		dataType: "json",
-		success: function (response){
-			console.log(response);
+	var userListSong = listSongs[currentUser.name] || {};
 
-			response.items.forEach(function (album){
-				var elementAux = $("<div class='album'>");
-				var imageAlbum = $("<img class='image-album'>").attr("src", album.images[0].url);
-				elementAux.append(imageAlbum);
-				elementAux.append($("<h1 class='album-name'>").text(album.name));
-				//requestTrack(album, element);
-				elementAux.on("click", function(){
-					requestTrack(album);
-				});
+	console.log(userListSong);
 
-				//element.append(elementAux);
-				$(".container-artist").append(elementAux);
-			});
+	for(song in userListSong){
+		var songElement = $("<li class='track-song'>");
+		var songLink = $("<p class='link-song' style='display:inline-block;margin-right:1em;'>");
+		songLink.text(song);
+		songElement.append(songLink);
+		songLink.on("click", function(){
+			$('iframe').attr("src", "https://embed.spotify.com/?uri="+userListSong[song]);
+		});
 
-			/*
-			var albumID = response.items[0].id;
-			requestTrack(albumID);*/
-		},
-		error: function (response){
-			console.log(response);
-		}
-	});
-}
-
-function requestTrack(album, element){
-	$.ajax({
-		url: "https://api.spotify.com/v1/albums/"+album.id,
-		dataType: "json",
-		success: function (response){
-			var elementAux = $("<ul class='track-list'>");
-
-			response.tracks.items.forEach(function(song){
-				var songElement = $("<li class='track-song'>");
-				var songLink = $("<a class='link-song'>").attr("href", song.preview_url);
-				songLink.attr("target", "_blank");
-				songLink.text(song.name);
-				songElement.append(songLink);
-				elementAux.append(songElement);
-
-			});
-			$(".songtrack-list").empty();
-			$(".songtrack-list").append(elementAux);
-			//console.log(response.tracks.items[0].preview_url); //recorrerlo con un for
-		},
-		error: function (response){
-			console.log(response);
-		}
-	});
+		$('.songtrack-list').append(songElement);
+	}
 }
 
 $(document).ready(function(){
@@ -135,7 +122,9 @@ $(document).ready(function(){
 		var artist = $('#input-artist').val();
 		requestAjax(artist, "artist");
 	});
+
+	$("#button-fav-list").on("click", function(e){
+		$('.songtrack-list').empty();
+		getFavouriteList();
+	});
 });
-
-
-
